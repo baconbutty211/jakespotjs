d4fa1fb13665dc767ec3114e1191b0cdd74936fe
Jake <jleiser211@gmail.com>
2023-08-03 22:07:06 +0100

    more leftover convex crap

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.css";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
-
-const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ConvexProvider client={convex}>
-      <App />
-    </ConvexProvider>
+    <App />
   </React.StrictMode>
 );
