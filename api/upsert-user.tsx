0e9fbd755ff3b8a76dd52e2511853680959963c5
Jake <jleiser211@gmail.com>
2023-09-27 17:50:50 +0100

    added .tsx extension back to upsert-user

--- api/upsert-user.tsx
+++ api/upsert-user.tsx
@@ -1,7 +1,7 @@
 // @ts-ignore
-import * as database from './database';
+import * as database from './database.tsx';
 // @ts-ignore
-import * as schema from './schema';
+import * as schema from './schema.tsx';
 import { VercelRequest, VercelResponse } from '@vercel/node';
  
 // Recieves user details (email, access token, refresh token, spotify_user_id). If user exists: current record is updated. if user does not exist: new record is created. Returns new/updated user record.
