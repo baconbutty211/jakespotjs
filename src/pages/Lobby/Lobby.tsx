d3cb803a896637b71a0e36709096db230514db24
Jake <jleiser211@gmail.com>
2024-09-16 03:21:19 +0100

    remove pusher api from master branch

--- src/pages/Lobby/Lobby.tsx
+++ src/pages/Lobby/Lobby.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { Player } from "../../../api/schema";
 import Title from "../../components/Title";
 import * as api from "../../requests/Backend";
-import Pusher from 'pusher-js';
 import { useCookies } from "react-cookie";
 import Button from "../../components/Button";
 import { updateGame } from "../../../api/database";
@@ -16,22 +15,6 @@ export default function Lobby() {
     useEffect(() => {
         // Fetch initial list of players when the component mounts
         fetchPlayers();
-        
-
-        const pusher = new Pusher(import.meta.env.VITE_PUSHER_KEY, {
-            cluster: import.meta.env.VITE_PUSHER_CLUSTER
-        });
-            
-        const channel = pusher.subscribe("JakeSpotJS");
-        channel.bind("new_player", (newPlayer: Player) => {
-            console.log(`New player event received: ${newPlayer}`);
-            // Fetch updated list of players when the player_join event is received
-            fetchPlayers();
-        });
-        
-        return () => {
-            pusher.unsubscribe("JakeSpotJS");
-        };
     }, []);
 
     async function fetchPlayers() {
