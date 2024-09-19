43f4cc88b8da41039b314e76b4d031f7066bd992
Jake <jleiser211@gmail.com>
2024-09-20 00:37:00 +0100

    Added some more attributes to custom Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,11 +3,12 @@ interface Props {
   color?: string;
   onClick?: () => void;
   visible?: boolean;
+  disabled?: boolean;
 }
 
-export default function Button({ children, onClick, color = "success", visible = true }: Props) {
+export default function Button({ children, onClick, color = "success", visible = true, disabled=false }: Props) {
   return (
-    <button onClick={onClick} className={"btn btn-primary btn-" + color} style={{opacity: visible ? 100 : 0}}>
+    <button onClick={onClick} className={"btn btn-primary btn-" + color} style={{opacity: visible ? 100 : 0}} disabled={disabled}>
       {children}
     </button>
   );
