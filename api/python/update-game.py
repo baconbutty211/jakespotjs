f59ea2e71b501be8109601caf2855a6d631f5b9a
Jake <jleiser211@gmail.com>
2024-09-19 19:51:22 +0100

    Sets new random song when game state switches to 'guessing'

--- api/python/update-game.py
+++ api/python/update-game.py
@@ -3,6 +3,8 @@ from http.server import BaseHTTPRequestHandler
 import logging
 import psycopg2
 import os
+import requests
+import random
 
 
 def get_connection():
@@ -20,6 +22,112 @@ def get_connection():
     )
 
 
+def get_random_player(game_id: int):
+    # Send SQL query to create a new game
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            # Does player exist?
+            cur.execute(
+                """SELECT * FROM players 
+                WHERE game_id = %(game_id)s
+                ORDER BY RANDOM()
+                LIMIT 1;
+                """,
+                {"game_id": game_id},
+            )
+            result = cur.fetchone()
+    return {
+        "id": result[0],
+        "user_id": result[1],
+        "game_id": result[2],
+        "spotify_playlist_id": result[3],
+        "score": result[4],
+    }
+
+
+def get_user_access_token(user_id: int):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            # Does player exist?
+            cur.execute(
+                """SELECT * FROM users 
+                WHERE id = %(user_id)s
+                ORDER BY RANDOM()
+                LIMIT 1;
+                """,
+                {"user_id": user_id},
+            )
+            result = cur.fetchone()
+            user = {
+                "id": result[0],
+                "spotify_access_token": result[2],
+                "spotify_refresh_token": result[3],
+                "spotify_user_id": result[4],
+            }
+    return user["spotify_access_token"]
+
+
+def get_spotify_playlist(access_token, playlist_id):
+    url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()  # returns the playlist data in JSON format
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return None
+
+
+def update_game(game_id: int, state: str, new_song_id: int = None):
+    # Update the game record in the database
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            if new_song_id == None:
+                cur.execute(
+                    """
+                    UPDATE games
+                    SET state = %(state)s
+                    WHERE id = %(id)s
+                    RETURNING *
+                """,
+                    {"id": game_id, "state": state},
+                )
+            else:
+                cur.execute(
+                    """
+                    UPDATE games
+                    SET state = %(state)s, current_song_id = %(new_song_id)s
+                    WHERE id = %(id)s
+                    RETURNING *
+                """,
+                    {"id": game_id, "state": state, "new_song_id": new_song_id},
+                )
+            result = cur.fetchone()
+    print(f"Updated game to {result}")
+    return {
+        "id": result[0],
+        "state": result[1],
+        "current_song_id": result[2],
+    }
+
+
+def set_song(player_id: int, spotify_track_id: str):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO songs (spotify_track_id, player_id)
+                VALUES (%(spotify_track_id)s, %(player_id)s)
+                RETURNING *
+            """,
+                {"spotify_track_id": spotify_track_id, "player_id": player_id},
+            )
+            result = cur.fetchone()
+    return {"id": result[0], "spotify_track_id": result[1], "player_id": result[2]}
+
+
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
         try:
@@ -31,19 +139,16 @@ class handler(BaseHTTPRequestHandler):
             print(request_data)
 
             # Extract data from request
-            id = request_data.get("game_id")
-            state = request_data.get("state")
-            new_song_id = request_data.get("new_song_id")
+            game_id: int = int(request_data.get("game_id"))
+            state: str = request_data.get("state")
 
-            if not id:
-                raise ValueError("id required.")
-            if not state and not new_song_id:
-                raise ValueError("state or new song id required.")
+            if not game_id:
+                raise ValueError("game id required.")
 
             # If the state is "scoring", score the players
             if state == "scoring":
                 raise NotImplementedError("Not implemented scoring")
-                guesses = database.find_latest_guesses_by_game_id(id)
+                guesses = database.find_latest_guesses_by_game_id(game_id)
                 if not guesses:
                     raise ValueError(
                         "Attempted to score when no players have guessed. (Guesses[] empty)"
@@ -54,25 +159,26 @@ class handler(BaseHTTPRequestHandler):
                     if guess["is_correct"]:
                         database.increment_player_score(guess["player_id"])
 
-            # Update the game record in the database
-            with get_connection() as conn:
-                with conn.cursor() as cur:
-                    cur.execute(
-                        """
-                        UPDATE games
-                        SET state = %(state)s, current_song_id = %(new_song_id)s
-                        WHERE id = %(id)s
-                        RETURNING *
-                    """,
-                        {"id": id, "state": state, "new_song_id": new_song_id},
-                    )
-                    result = cur.fetchone()
-            print(f"Newly created game {result}")
-            updated_game = {
-                "id": result[0],
-                "state": result[1],
-                "current_song_id": result[2],
-            }
+            # If the state is "guessing", set new current_song_id
+            if state == "guessing":
+                random_player = get_random_player(game_id)
+                user_id = int(random_player["user_id"])
+                spotify_playlist_id = random_player["spotify_playlist_id"]
+                random_player_access_token = get_user_access_token(user_id)
+                random_player_playlist = get_spotify_playlist(
+                    random_player_access_token, spotify_playlist_id
+                )
+                # Select random spotify song id from the playlist json
+                spotify_song_ids = []
+                for song in random_player_playlist["tracks"]["items"]:
+                    spotify_song_ids.append(song["track"]["id"])
+                random_spotify_song_id = spotify_song_ids[
+                    random.randint(0, len(spotify_song_ids))
+                ]
+                new_song = set_song(random_player["id"], random_spotify_song_id)
+                updated_game = update_game(game_id, state, new_song["id"])
+            else:
+                updated_game = update_game(game_id, state)
 
             # Log the updated game record
             logging.info(updated_game)
