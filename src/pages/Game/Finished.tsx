982589ee78f8b8203f7d938cc40e348a314aa8ce
Jake <jleiser211@gmail.com>
2024-09-23 02:20:51 +0100

    Implemented final scoreboard

--- src/pages/Game/Finished.tsx
+++ src/pages/Game/Finished.tsx
@@ -11,8 +11,13 @@ interface Props {
     }
   }
 export default function Finished({players, cookies}: Props) {
+    let best_player: schema.Player = {id: -1, user_id: -1, game_id: -1, spotify_playlist_id: "", score: -1, image:"", username: "Doesn't Exist"};
+    players.forEach((player) => {
+        if(player.score > best_player.score) 
+            best_player = player;
+    });
     return (<>
-        <h2> Congratulations ___ won with a score of: NotImplemented </h2>
         <Scoring players={players} cookies={cookies} final={true}/>
+        <h2> Congratulations {best_player.username} won with a score of: {best_player.score} </h2>
     </>)
 }
