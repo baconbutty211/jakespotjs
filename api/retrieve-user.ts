314c82ca2ba5e9ebb420f582ba4f8351f5558ed7
Jake <jleiser211@gmail.com>
2025-06-01 21:19:21 +0100

    Fixed /retrieve-user endpoint

--- api/retrieve-user.ts
+++ api/retrieve-user.ts
@@ -3,12 +3,12 @@ import * as database from './database.js';
 // @ts-ignore
 import * as schema from './schema.js';
 import { VercelRequest, VercelResponse } from '@vercel/node';
- 
+
 // Returns all players in the game.
-// Body : { id }
+// Body : { user_id }
 export default async function POST(request: VercelRequest, response: VercelResponse) {
     try {
-        const id = request.body.id as schema.User["id"];
+        const id = request.body.user_id as schema.User["id"];
 
         if (!id) {
             throw new Error("user id required.");
@@ -16,7 +16,7 @@ export default async function POST(request: VercelRequest, response: VercelRespo
 
         const user: schema.User[] = await database.findUserById(id);
         response.setHeader('Content-Type', 'application/json');
-        response.status(200).json( user[0] );
+        response.status(200).json(user[0]);
     }
     catch (error) {
         console.error(error);
