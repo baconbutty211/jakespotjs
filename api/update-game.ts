187b17319fef8f17e8dd2d9d1cb40d8e1c88b8f8
Jake <jleiser211@gmail.com>
2025-06-01 23:27:03 +0100

    Sets new random song at the beginning of every round of guessing

--- api/update-game.ts
+++ api/update-game.ts
@@ -6,30 +6,26 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 
 
 // Recieves user details (email, access token, refresh token). If user exists: current record is updated. if user does not exist: new record is created. Returns new/updated user record.
-// Body: { game_id, state?, new_song_id? }
+// Body: { game_id, state? }
 export default async function POST(request: VercelRequest, response: VercelResponse) {
     try {
-        const id = request.body.game_id as schema.Game["id"];
+        const game_id = request.body.game_id as schema.Game["id"];
         const state = request.body.state as schema.Game["state"];
-        const new_song_id = request.body.new_song_id as schema.Song["id"];
 
         // console.log(request.body);
         // console.log(request.body["game_id"]);
         // console.log(request.body["state"]);
 
         const updateGameData = {} as schema.UpdateableGame;
-        if (!id) {
+        if (!game_id) {
             throw new Error("id required.");
         }
         else {
-            updateGameData.id = id;
+            updateGameData.id = game_id;
         }
 
         if (state) {
             updateGameData.state = state;
-        }
-        else if (new_song_id) {
-            updateGameData.current_song_id = new_song_id;
         }
         else {
             throw new Error("state or new song id required.");
@@ -37,7 +33,7 @@ export default async function POST(request: VercelRequest, response: VercelRespo
 
 
         if (state === "scoring") { // Score for the players
-            const guesses = await database.findLatestGuessesByGameId(id);
+            const guesses = await database.findLatestGuessesByGameId(game_id);
             if (guesses.length == 0) {
                 throw new Error("Attempted to score when no players have guessed. (Guesses[] empty)");
             }
@@ -49,6 +45,13 @@ export default async function POST(request: VercelRequest, response: VercelRespo
                     }
                 });
             }
+        }
+        if (state === "guessing") {
+            // Set new current song for the game
+            const players = await database.findPlayersByGameId(game_id);
+            const randomPlayer = players[Math.floor(Math.random() * players.length)];
+            const song = await database.findSongsByPlayerId(randomPlayer.id);
+            updateGameData.current_song_id = song[0].id;
         }
         const updatedGame: schema.Game = await database.updateGame(updateGameData);
         console.log(updatedGame);
